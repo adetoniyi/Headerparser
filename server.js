5360437a37c9ec86ec0241fbeec0c13f6f583022
adetoniyi <adetoniyi04@gmail.com>
2025-05-02 12:47:01 +0100

    Header

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
 
 // API endpoint
 app.get("/api/whoami", (req, res) => {
-  const ipaddress = req.ip;
+  const ipaddress = req.headers['x-forwarded-for']?.split(',')[0] || req.socket.remoteAddress;
   const language = req.headers["accept-language"];
   const software = req.headers["user-agent"];
 
